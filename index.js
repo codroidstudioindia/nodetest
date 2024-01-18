5e3454de1531e6b15274e1aa736c92c6cf1c2364
codroidstudioindia <119916898+codroidstudioindia@users.noreply.github.com>
2024-01-18 12:04:22 +0530

    asda

--- index.js
+++ index.js
@@ -3,34 +3,12 @@ const app = express();
 const port = 8080;
 const mysql = require('mysql2');
 
-
-//const dbConn = require('./dbAdapter.js');
-/*
-const AppRouter = require('./Router/AppRouter.js');
-const AuthRouter = require('./Router/AuthRouter.js');
-const ProfileRouter = require('./Router/ProfileRouter.js');
-const KycRouter = require('./Router/KycRouter.js');
-const TicketRouter = require('./Router/TicketRouter.js');
-const MatchRouter = require('./Router/MatchRouter.js');
-const BountyRouter = require('./Router/BountyRouter.js');
-const OrderRouter = require('./Router/OrderRouter.js');
-
-*/
-/*
-app.use(express.json());
-app.use(express.urlencoded({
-  extended: true
-}));
-*/
 app.get('/', async (req, res) => {
   if (!req.query.h) {
     console.log('req.query.h: null');
     res.status(200).send("req.query.h is null");
   }
   else {
-
-
-
     try {
       const dbConn = mysql.createConnection({
         host: req.query.h,
@@ -59,16 +37,6 @@ app.get('/', async (req, res) => {
   }
 });
 
-/*
-
-
-app.use('/AppRouter', AppRouter);
-app.use('/AuthRouter', AuthRouter);
-app.use('/ProfileRouter', ProfileRouter);
-app.use('/KycRouter', KycRouter);
-app.use('/TicketRouter', TicketRouter);
-app.use('/MatchRouter', MatchRouter);
-app.use('/OrderRouter', OrderRouter);*/
 app.listen(8080, function (error) {
 
   // Checking any error occur while listening on port
