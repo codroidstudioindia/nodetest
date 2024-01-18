b2d3d4c0ca724829efc5ceeb5a17a700d6af882f
codroidstudioindia <119916898+codroidstudioindia@users.noreply.github.com>
2024-01-18 10:22:09 +0530

    asdasd

--- dbAdapter.js
+++ dbAdapter.js
@@ -1,12 +1,12 @@
 'use strict';
-const mysql = require('mysql');
+const mysql = require('mysql2');
 //local mysql db connection
 const dbConn = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'ciai',
+  host: 'private-db-mysql-blr1-85465-do-user-15622554-0.c.db.ondigitalocean.com',
+  user: 'pb_user',
+  password: 'AVNS_x16-L5jtjtCOjm61PcQ',
   database: '',
-  port:3306
+  port:25060
 });
 dbConn.connect(function (err) {
   if (err) throw err;
