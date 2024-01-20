0beecd346cb2f2e000084ca5b76621db9ac8f909
codroidstudioindia <119916898+codroidstudioindia@users.noreply.github.com>
2024-01-20 23:57:31 +0530

    asd

--- index.js
+++ index.js
@@ -1,44 +1,14 @@
 const express = require('express');
 const app = express();
-const port = 8080;
+const port = 80;
 const mysql = require('mysql2');
 
 
 app.get('/', async (req, res) => {
-  if (!req.query.h) {
-    console.log('req.query.h: null');
-    res.status(200).send("req.query.h is null");
-  }
-  else {
-    try {
-      const dbConn = mysql.createConnection({
-        host: req.query.h,
-        user: req.query.u,
-        password: req.query.ps,
-        database: req.query.d,
-        port: req.query.p
-      });
-
-
-      dbConn.connect(function (err) {
-        if (err) {
-          res.status(520).send("error: \n");
-          console.log('err', err);
-          //throw err;
-        } else {
-          console.log('connected');
-
-          res.status(200).send("db connected: \n");
-        }
-      });
-    } catch (error) {
-      console.log('error', error);
-      //res.status(500).send("error: \n", error);
-    }
-  }
+ res.status(200).send('homepage');
 });
 
-app.listen(8080, function (error) {
+var server = app.listen(port, function (error) {
 
   // Checking any error occur while listening on port
   if (error) {
@@ -46,7 +16,7 @@ app.listen(8080, function (error) {
   }
   // Else sent message of listening
   else {
-    console.log('Server is listening on port' + port);
+    console.log('Server is listening on port http://localhost:'+server.address().port);
   }
 });
 
